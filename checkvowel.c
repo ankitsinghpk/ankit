ba192202c8a8d1d1ed46b166ee62ac36c214f4c1
ankitsinghpk <42380453+ankitsinghpk@users.noreply.github.com>
2019-07-09 07:14:26 -0700

    Update checkvowel.c

--- checkvowel.c
+++ checkvowel.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+  #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 int main()
 {
-char str[20];
-fgets(str, sizeof str,stdin);
+char str;
+scanf("%c",&str);
 if((str>='a'&&str<='z')||(str>='A'&&str<='Z'))
 {
-if((str='a'&&str=='i'&&str=='e'&&str=='o'&&str=='u')||(str=='A'&&str=='E'&&str=='I'&&str=='O'&&str=='U'))
+if((str='a'||str=='i'||str=='e'||str=='o'||str=='u')||(str=='A'||str=='E'||str=='I'||str=='O'||str=='U'))
 printf("Vowel");
 else
 printf("Consonant");
