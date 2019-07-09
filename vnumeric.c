97294d2c507748686e4aa08c0ee33f909099d238
ankitsinghpk <42380453+ankitsinghpk@users.noreply.github.com>
2019-07-08 21:13:43 -0700

    Create vnumeric.c

---
+++ vnumeric.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(void) {
+	// your code goes here
+	char str[20];
+	int l,i,flag=0;
+	fgets(str, sizeof str, stdin);
+	l=strlen(str);
+	for(i=0;i<=l;i++)
+	{
+		if((str[i]>='a'&& str[i]<='z')||(str[i]>='A'&& str[i]<='Z'))
+		flag=1;
+	}
+	if(flag==1)
+	printf("no");
+	return 0;
+}
