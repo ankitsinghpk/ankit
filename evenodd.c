9b9af75202c70c5dfc2e25d8a874e72d956efb9f
ankitsinghpk <42380453+ankitsinghpk@users.noreply.github.com>
2019-07-09 01:23:42 -0700

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+if(n%2==0)
+printf("Even");
+else
+printf("Odd");
+}
+else
+printf("invalid");
+return 0;
+}
