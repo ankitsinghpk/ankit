539682c19cd06266dbe6f0834d0c248b158a0eba
ankitsinghpk <42380453+ankitsinghpk@users.noreply.github.com>
2019-07-15 10:41:22 +0530

    Create player39.c

---
+++ player39.c
@@ -0,0 +1,23 @@
+  #include <stdio.h>
+#include<string.h>
+#include<math.h>
+
+int main(void) {
+	
+	int n,k;
+	float b;
+	// your code goes here
+	 scanf("%d",&n);
+	 
+	 b=sqrt(n);
+	 if(2==b)
+	 printf("yes");
+	 else
+	 printf("no");
+	 
+	 
+	 
+	 
+	 
+	return 0;
+}
